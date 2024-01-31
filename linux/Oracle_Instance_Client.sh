68020b793e2c12b9281b4b59abc9a2d092cb51ab
Lucas Macedo <132277653+lucas-macedo-dev@users.noreply.github.com>
2024-01-31 07:53:35 -0300

    Atualizar o Oracle_Instance_Client.sh

--- linux/Oracle_Instance_Client.sh
+++ linux/Oracle_Instance_Client.sh
@@ -1,1 +1,11 @@
 
+   apt-get update \
+&& apt-get install -y \
+    libaio1 \
+    unzip \
+&& mkdir /opt/oracle \
+&& curl $INSTANTCLIENT_URL --output "instantclient.zip" \
+&& unzip instantclient.zip -d /opt/oracle \
+&& rm instantclient.zip \
+&& find "/opt/oracle" -type "d" -name "instantclient_*" > /etc/ld.so.conf.d/oracle-instantclient.conf \
+&& ldconfig
